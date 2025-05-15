e7b310e30d0da7ea468606859a059a417fc5c12d
MohammadHosseinRabie <mohammad.hossein.rabie@gmail.com>
2025-05-15 10:33:51 +0330

    fix fields page

--- src/app/(features)/projects/[id]/fields/page.jsx
+++ src/app/(features)/projects/[id]/fields/page.jsx
@@ -4,18 +4,7 @@ import { Box, Spinner, Stack } from "@chakra-ui/react";
 
 export default function Fields() {
 
-  if (isLoading) {
-    return (
-      <Box p={"20"}>
-        <Spinner size={"md"} />
-        در حال بارگذاری...
-      </Box>
-    );
-  }
-
-  if (isError) {
-    return <Box p={"20"}>خطا در بارگذاری داده‌ها!</Box>;
-  }
+  
 
   return (
     <Stack>
