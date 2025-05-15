fd848418aca392f1636249681b4d70d967e3294a
MohammadHosseinRabie <mohammad.hossein.rabie@gmail.com>
2025-05-15 14:57:57 +0330

    create hook for edit fields name

---
+++ src/hooks/use-edit-field.js
@@ -0,0 +1,9 @@
+import { axiosClient } from "./axios-client"
+
+
+const putEditField = async ({ fieldId, projectId, name }) => {
+    const { data } = await axiosClient.put(`/projects/${projectId}/custom-fields/${fieldId}`, { name })
+    return data;
+};
+
+export default putEditField;
