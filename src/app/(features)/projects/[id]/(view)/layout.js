5e2d53d883972650e35c69848de60bdc75e7e25b
MohammadHosseinRabie <mohammad.hossein.rabie@gmail.com>
2025-05-18 14:19:41 +0330

    create view layout & page

---
+++ src/app/(features)/projects/[id]/(view)/layout.js
@@ -0,0 +1,18 @@
+import { Button, Tabs } from "@chakra-ui/react";
+import { LuPlus } from "react-icons/lu";
+
+export default function Layout({ children }) {
+  return (
+    <Tabs.Root defaultValue="members" p={"14"} variant={"outline"}>
+      <Tabs.List dir="rtl">
+        <Tabs.Trigger value="Table">Table</Tabs.Trigger>
+        <Button alignSelf="center" ms="2" size="2xs" variant="outline">
+          <LuPlus /> Add Tab
+        </Button>
+      </Tabs.List>
+      <Tabs.Content value="Table" dir="rtl">
+        {children}
+      </Tabs.Content>
+    </Tabs.Root>
+  );
+}
---
+++ src/app/(features)/projects/[id]/(view)/page.jsx
@@ -0,0 +1,9 @@
+"use client"
+
+import { Stack } from "@chakra-ui/react"
+
+export default function view() {
+  return (
+    <Stack></Stack>
+  )
+}
